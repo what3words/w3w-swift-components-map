9823e608625044c42776a11b745c0ffc7b018021
Dave Duprey <dave@what3words.com>
2025-04-27 16:22:15 +0100

    added default max grid size

--- Sources/W3WSwiftComponentsMap/Presets/PresetMapScales.swift
+++ Sources/W3WSwiftComponentsMap/Presets/PresetMapScales.swift
@@ -5,9 +5,14 @@
 //  Created by Dave Duprey on 08/01/2025.
 //
 
+import W3WSwiftCore
 
 public extension W3WMapScale {
   
+  /// the default scale to zoom to if none is provided for certain calls
   static var standardZoom = W3WMapScale(pointsPerMeter: 4.0)
+
+  /// the maximum allowed grid size
+  static var maxGridSize: W3WDistance = W3WBaseDistance(meters: 4000.0)
   
 }
